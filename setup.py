a1f0d2cc366693b1393855d96bd15be01cfae888
Itai Guli <click.itai@gmail.com>
2020-07-05 14:26:48 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'guli',
+  packages = ['guli'],
+  version = '0.1',
+  license='MIT',
+  description = 'Passing variables between python processes',
+  author = 'Itai Guli',
+  author_email = 'app.redalert@gmail.com',
+  url = 'https://github.com/user/itaiguli',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['GuliVariable', 'guli', 'multiprocessing'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
